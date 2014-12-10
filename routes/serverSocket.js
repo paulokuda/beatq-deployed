65c92594af0852e2c7e092250a96055c840b61fe
Paul Okuda <paulokuda@Pauls-MacBook-Pro-3.local>
2014-12-09 20:11:50 -0500

    asdf

--- routes/serverSocket.js
+++ routes/serverSocket.js
@@ -1,20 +1,12 @@
 var messageModel = require ('../models/message.js');
 var util = require("util");
-var mongoClient = require('mongodb').MongoClient;
+
 /*
  * This is the connection URL
  * Give the IP Address / Domain Name (else localhost)
  * The typical mongodb port is 27012
  * The path part (here "fallTest") is the name of the databas
  */
-var url = 'mongodb://localhost:27017/fallTest';
-var mongoDB; // The connected database
-// Use connect method to connect to the Server
-mongoClient.connect(url, function(err, db) {
-  if (err) doError(err);
-  console.log("Connected correctly to server");
-  mongoDB = db;
-});
 
 
 
