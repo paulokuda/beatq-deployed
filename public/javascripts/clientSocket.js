05709a2ce4b0c6db95e1b1488d6e45b2b41ce6be
Paul Okuda <paulokuda@CMU-845995.WV.CC.CMU.EDU>
2014-12-10 19:49:29 -0500

    fadsfadsfds

--- public/javascripts/clientSocket.js
+++ public/javascripts/clientSocket.js
@@ -57,27 +57,8 @@ $(document).ready(function(){
     
     var socket = io.connect(':8000/');
     socket.emit('index page');
-    // $('#setUsername').submit(function(e) {
-    //     e.preventDefault();
-    //     socket.emit('new user', $('#nickname').val(), function(data) {
-    //         if (data) {
-    //             // console.log(data);
-    //             // alert(JSON.stringify(nickBox));
-                    
-    //             $('#first-page').slideUp();
-    //             $('.main-container').show();
-
-    //         }
-    //         else {
-    //             alert("That username has already been chosen! Please enter another.")
-    //         }
-    //     });
-    //     $('#nickname').val('');
-    // });
-    $('#new-room').onclick = function(){
-        // $('#first-page').slideToggle();
-        // $('.main-container').show();
-        // $('#nickWrap').show();
+    $('#setUsername').submit(function(e) {
+        e.preventDefault();
         socket.emit('new user', $('#nickname').val(), function(data) {
             if (data) {
                 // console.log(data);
@@ -92,8 +73,27 @@ $(document).ready(function(){
             }
         });
         $('#nickname').val('');
+    });
+    // $('#new-room').onclick = function(){
+    //     // $('#first-page').slideToggle();
+    //     // $('.main-container').show();
+    //     // $('#nickWrap').show();
+    //     socket.emit('new user', $('#nickname').val(), function(data) {
+    //         if (data) {
+    //             // console.log(data);
+    //             // alert(JSON.stringify(nickBox));
+                    
+    //             $('#first-page').slideUp();
+    //             $('.main-container').show();
 
-    }
+    //         }
+    //         else {
+    //             alert("That username has already been chosen! Please enter another.")
+    //         }
+    //     });
+    //     $('#nickname').val('');
+
+    // }
 
 
     $('form').submit(function(){
