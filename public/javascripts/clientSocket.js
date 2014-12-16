95195a5cc12742c39568797b72d7e4ab71b3afbd
Paul Okuda <paulokuda@Pauls-MacBook-Pro-3.local>
2014-12-16 02:32:37 -0500

    asdf

--- public/javascripts/clientSocket.js
+++ public/javascripts/clientSocket.js
@@ -1,6 +1,7 @@
 var theQ = [];
 var firstHalfUrl = "<iframe width=\u0022100%\u0022 height=\u0022400\u0022src=\u0022http://www.youtube.com/embed/";
 var secondHalfUrl = "?autoplay=0\u0022></iframe>";
+var favorites = [];
 
 
 
