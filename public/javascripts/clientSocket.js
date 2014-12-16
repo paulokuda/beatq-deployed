2cd9a506cfca8365262facd499a5a8d509b57307
Paul Okuda <paulokuda@Pauls-MacBook-Pro-3.local>
2014-12-15 20:54:59 -0500

    a

--- public/javascripts/clientSocket.js
+++ public/javascripts/clientSocket.js
@@ -5,7 +5,7 @@ var secondHalfUrl = "?autoplay=0\u0022></iframe>";
 
 
 $(document).ready(function(){
-    alert(JSON.stringify(nicknames));
+    alert(nicknames.value);
     // socket.emit('index page');
     $('.collapsible').collapsible();
     $('#collapse').onclick = function() {
